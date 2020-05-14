c439c58cec642828c685a21757b09c67ab744926
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-05-14 22:39:09 +0300

    Created service to work with themoviedb API. Added method to fetch daily trending movies

---
+++ src/services/themoviedb-api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '9fa4665f2507db381929fa4018205168';
+// api_key=9fa4665f2507db381929fa4018205168
+
+export const fetchDailyTrendingMovies = () => {
+  return axios
+    .get(`${BASE_URL}trending/movie/day?api_key=${API_KEY}`)
+    .then(responce => responce.data);
+};
+
+export const fetchMovieById = () => null;
