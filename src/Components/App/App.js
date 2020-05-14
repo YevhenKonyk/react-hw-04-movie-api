4c2502b306ecec5197910b8a295209cb913044c9
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-05-14 16:32:00 +0300

    Created react app

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <h1>React app</h1>
+      </>
+    );
+  }
+}
