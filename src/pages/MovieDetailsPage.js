e0b00578439f91a69ad507c147a1d7d429085d89
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-05-17 19:27:15 +0300

    Added functionality to display movie details

--- src/pages/MovieDetailsPage.js
+++ src/pages/MovieDetailsPage.js
@@ -1,16 +1,36 @@
+/* eslint-disable react/static-property-placement */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { fetchMovieById } from '../services/themoviedb-api';
+import Movie from '../Components/MovieDetails/MovieDetails';
 
 const getMovieIdFromProps = props => props.match.params.movieId;
 
 export default class MovieDetailsPage extends Component {
-  state = {};
+  static propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+    }).isRequired,
+  };
+
+  state = {
+    movie: null,
+  };
+
+  componentDidMount() {
+    const currentMovieId = getMovieIdFromProps(this.props);
+
+    fetchMovieById(currentMovieId).then(movie => this.setState({ movie }));
+  }
+
+  handleGoBack = () => {
+    this.props.history.push('/');
+  };
 
   render() {
-    return (
-      <>
-        Movie Details Page
-        {getMovieIdFromProps(this.props)}
-      </>
-    );
+    const { movie } = this.state;
+
+    return <>{movie && <Movie item={movie} onGoBack={this.handleGoBack} />}</>;
   }
 }
