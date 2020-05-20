1e6d2bb7d745628ff7d4e4c9a0612e9f8d530830
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-05-20 15:13:08 +0300

    Removed unnecessary comments

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,8 +7,6 @@ import HomePage from '../../pages/HomePage';
 import MoviesPage from '../../pages/MoviesPage';
 import MovieDetailPage from '../../pages/MovieDetailsPage';
 import NotFoundPage from '../../pages/NotFoundPage';
-// import MovieCreditsPage from '../../pages/MovieCreditsPage';
-// import MovieReviewsPage from '../../pages/MovieReviewsPage';
 
 export default class App extends Component {
   state = {};
@@ -19,8 +17,6 @@ export default class App extends Component {
         <Navigation />
         <Switch>
           <Route path="/" exact component={HomePage} />
-          {/* <Route path="/movies/:movieId/credits" component={MovieCreditsPage} /> */}
-          {/* <Route path="/movies/:movieId/reviews" component={MovieReviewsPage} /> */}
           <Route path="/movies/:movieId" component={MovieDetailPage} />
           <Route path="/movies" component={MoviesPage} />
           <Route component={NotFoundPage} />
